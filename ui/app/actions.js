e72ac30382c2cd893cbeff6640718491f0dd1b4a
Dan <danjm.com@gmail.com>
2018-06-20 13:18:23 -0230

    Remove send_ directory, revert to just having send
    Revert accidentally changed constants.
    
    Require defaults in ens-input, gas-fee-display and confirm screens.

--- ui/app/actions.js
+++ ui/app/actions.js
@@ -7,7 +7,7 @@ const {
   calcTokenBalance,
   estimateGas,
   estimateGasPriceFromRecentBlocks,
-} = require('./components/send_/send.utils')
+} = require('./components/send/send.utils')
 const ethUtil = require('ethereumjs-util')
 const { fetchLocale } = require('../i18n-helper')
 const log = require('loglevel')
