937e5d0354d8656c76514e59e8c3fd5785fde145
siemarell <siemarell@gmail.com>
2018-07-02 17:20:36 +0300

    started network

--- app/scripts/controllers/wavesNetwork/network.js
+++ app/scripts/controllers/wavesNetwork/network.js
@@ -2,14 +2,27 @@ const EventEmitter = require('events')
 const ObservableStore = require('obs-store')
 const ComposedStore = require('obs-store/lib/composed')
 const log = require('loglevel')
+const Waves = require('./wavesPatchedApi')
 
 module.exports = class WavesNetworkController extends EventEmitter {
-    constructor(opts = {}){
-        super()
-    }
+  constructor(opts = {}) {
+    super()
+    this.txStore = opts.networkStore || new ObservableStore({
+      transactions: []
+    })
+  }
 
-    _configureProvider(opts){
-        const {nodeUrl} = opts
-        
-    }
+  addUnapprovedTx(tx){
+    const newState = [...this.txStore.getState(), {status: 'unapproved', tx: tx}]
+    this._saveState(newState)
+  }
+
+  updateTxStatus(txId, status){
+
+  }
+
+  _saveState(newState){
+    this.txStore.updateState(newState)
+  }
+
 }
